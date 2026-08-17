7648780903022b28e36b5afd5c2475af1c1899e3
mohamedsalah333555 <mohamedsalh333555@gmail.com>
2026-08-17 07:44:38 +0300

    feat: add reusable VSPBackButton component with glassmorphism and haptic feedback

--- lib/shared/widgets/vsp_back_button.dart
+++ lib/shared/widgets/vsp_back_button.dart
@@ -1,28 +1,98 @@
+import 'dart:ui';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:iconsax_flutter/iconsax_flutter.dart';
 import '../../core/ui/tokens/vsp_tokens.dart';
 
-class VSPBackButton extends StatelessWidget {
+/// 🔘 زر الرجوع الموحد لتطبيق VSP (تصميم زجاجي دائري مع أنيميشن ولمس تفاعلي)
+class VSPBackButton extends StatefulWidget {
   final VoidCallback? onTap;
+  final Color? iconColor;
+  final double size;
+  final EdgeInsetsGeometry? margin;
 
-  const VSPBackButton({super.key, this.onTap});
+  const VSPBackButton({
+    super.key,
+    this.onTap,
+    this.iconColor,
+    this.size = 38.0,
+    this.margin,
+  });
+
+  @override
+  State<VSPBackButton> createState() => _VSPBackButtonState();
+}
+
+class _VSPBackButtonState extends State<VSPBackButton>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _scale;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 120),
+      vsync: this,
+      lowerBound: 0.90,
+      upperBound: 1.0,
+    )..value = 1.0;
+    _scale = Tween<double>(begin: 1.0, end: 0.90).animate(_controller);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     final isArabic = Localizations.localeOf(context).languageCode == 'ar';
-    return GestureDetector(
-      onTap: onTap ?? () => Navigator.pop(context),
-      child: Container(
-        padding: const EdgeInsets.all(10),
-        decoration: BoxDecoration(
-          color: VSPColors.surface,
-          shape: BoxShape.circle,
-          border: Border.all(color: VSPColors.borderLight),
-        ),
-        child: Icon(
-          isArabic ? Iconsax.arrow_right_1_copy : Iconsax.arrow_left_2_copy,
-          color: VSPColors.textPrimary,
-          size: 20,
+
+    return Container(
+      margin: widget.margin ?? const EdgeInsets.all(8),
+      child: ScaleTransition(
+        scale: _scale,
+        child: GestureDetector(
+          onTapDown: (_) => _controller.reverse(),
+          onTapUp: (_) => _controller.forward(),
+          onTapCancel: () => _controller.forward(),
+          onTap: () {
+            HapticFeedback.lightImpact();
+            if (widget.onTap != null) {
+              widget.onTap!();
+            } else {
+              Navigator.maybePop(context);
+            }
+          },
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(VSPRadius.full),
+            child: BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 16, sigmaY: 16),
+              child: Container(
+                width: widget.size,
+                height: widget.size,
+                decoration: BoxDecoration(
+                  color: VSPColors.glassSurface,
+                  shape: BoxShape.circle,
+                  border: Border.all(
+                    color: VSPColors.glassBorder,
+                    width: 1,
+                  ),
+                ),
+                child: Center(
+                  child: Icon(
+                    isArabic
+                        ? Iconsax.arrow_right_3_copy
+                        : Iconsax.arrow_left_2_copy,
+                    color: widget.iconColor ?? Colors.white,
+                    size: 15,
+                  ),
+                ),
+              ),
+            ),
+          ),
         ),
       ),
     );
