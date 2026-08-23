9c97f5d5a1b61de94a2c127f852d44b3d488a50a
mohamedsalah333555 <mohamedsalh333555@gmail.com>
2026-08-24 00:54:19 +0300

    feat: add Android build configuration, Gradle properties, and exclude flutter_secure_storage paths from IntelliJ project index

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -19,7 +19,7 @@ if (keystorePropertiesFile.exists()) {
 android {
     namespace = "app.vsp.sports"
     compileSdk = 35
-    ndkVersion = "27.0.12077973"
+    ndkVersion = "30.0.15729638"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
