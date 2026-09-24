6028b7048867e8c8bf4297a9a54361f4de3cc7d9
mohamedsalh333555 <mohamedsalh333555@gmail.com>
2026-09-24 14:43:29 +0300

    decouple full online payment from deposit feature flag

--- lib/features/player/widgets/booking/booking_confirmation_handler.dart
+++ lib/features/player/widgets/booking/booking_confirmation_handler.dart
@@ -5,7 +5,6 @@ import '../../../../core/providers/auth_provider.dart';
 import '../../../../core/providers/booking_provider.dart';
 import '../../../../core/repositories/booking_repository.dart';
 import '../../../../core/services/logger_service.dart';
-import '../../../../core/services/remote_config_service.dart';
 import '../../../../core/utils/vsp_feedback.dart';
 import '../../../../data/models.dart';
 import 'booking_payment_method_sheet.dart';
@@ -231,21 +230,6 @@ class BookingConfirmationHandler {
         b.endTime.isAfter(DateTime.now()));
 
     if (hasActiveCashBooking) {
-      final remoteConfig = Provider.of<RemoteConfigService>(context, listen: false);
-      final onlineEnabled = remoteConfig.isFeatureEnabled('online_payment_enabled');
-      if (!onlineEnabled) {
-        HapticFeedback.vibrate();
-        final isAr = Localizations.localeOf(context).languageCode == 'ar';
-        VSPFeedback.showError(
-          context,
-          isAr
-              ? 'عندك حجز كاش قائم، والحجز الجديد لازم يتدفع بالكامل أونلاين. الدفع الإلكتروني غير متاح حالياً.'
-              : 'You have an active cash booking. The new booking must be paid in full online, but online payment is currently unavailable.',
-        );
-        onLoadingChanged(false);
-        return;
-      }
-
       HapticFeedback.lightImpact();
       VSPFeedback.showInfo(
         context,
